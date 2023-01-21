801c0289d024363138d1013c7e9b2c362dffef5f
sahil6096 <sharmasha988.com>
2023-01-21 19:37:46 +0530

    addnew

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+//Initialization
+
 const express = require("express");
 const app = express();
-app.listen(5000);
+//App route
+app.get("/",function(req,res){
+    res.send("this is the homeapage")
+});
+app.get("/notes",function(req,res){
+    res.send("this is the Notes Page")
+});
+//starting the server on  a port
+
+app.listen(5000,function(){
+    console.log("server start at PORT: 5000");
+});
